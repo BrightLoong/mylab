d6106c46b2e70ef2e54ed9132be5cadae64999d3
BrightLoong <chenlong_cn@163.com>
2020-02-27 09:34:25 +0800

    提交

---
+++ spring-boot-learn/src/main/java/io/github/brightloong/spring/boot/learn/App.java
@@ -0,0 +1,12 @@
+package io.github.brightloong.spring.boot.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
